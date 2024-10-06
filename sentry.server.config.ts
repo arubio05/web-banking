a617733d9943cdb96c47b34e0c79081e29ef7ea2
arubio05 <arubio030509@gmail.com>
2024-10-05 21:38:01 -0700

    Implement auth, appwrite, and sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://27a45dd21035a6d96cfd1d99df247d70@o4508067719413760.ingest.us.sentry.io/4508067721576448",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
